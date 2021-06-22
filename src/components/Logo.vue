3453d795086082ed2400e43e9c3a2807db344f16
Dawid Schmidt <schmidtdawid@gmail.com>
2021-06-22 14:29:32 +0200

    add logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="logo">join.tsh.io</div>
+</template>
+
+<script>
+  export default {
+    name: 'Logo'
+  };
+</script>
+
+<style lang="scss">
+  #logo {
+    font-family: 'Nunito', sans-serif;
+    font-weight: 600;
+    font-size: 24px;
+    color: $black;
+  }
+</style>
